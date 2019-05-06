038f584f222d18ebcfcd93c22645ddb91b135178
Alessandro Canicatti <ales.canicatti@gmail.com>
2019-05-06 17:44:30 +0200

    Application can create meeting using latest participants

--- src/main/kotlin/ale7canna/randomhost/Application.kt
+++ src/main/kotlin/ale7canna/randomhost/Application.kt
@@ -1,9 +1,21 @@
 package ale7canna.randomhost
 
-class Application(private val communication: ICommunication) {
+class Application(
+    private val communication: ICommunication,
+    private val storage: IStorage<Meeting>
+) {
+
     fun createMeeting(): Meeting =
         Meeting(
             communication.askInput("Insert hosts"),
+            communication.askInput("Insert name"),
+            communication.askInput("Insert location"),
+            communication.askInput("Insert time")
+        )
+
+    fun createMeetingUsingLatestParticipants(): Meeting =
+        Meeting(
+            storage.restoreLatest().hosts,
             communication.askInput("Insert name"),
             communication.askInput("Insert location"),
             communication.askInput("Insert time")
--- src/test/kotlin/AppiicationTest.kt
+++ src/test/kotlin/AppiicationTest.kt
@@ -1,19 +1,16 @@
-import ale7canna.randomhost.Application
-import ale7canna.randomhost.Host
-import ale7canna.randomhost.ICommunication
-import ale7canna.randomhost.Meeting
+import ale7canna.randomhost.*
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
-import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.mockk
-import io.mockk.verify
 import java.time.LocalDateTime
 import java.time.Month
 
 class ApplicationTest: StringSpec() {
     init {
         val communication: ICommunication = mockk()
+        val storage: IStorage<Meeting> = mockk()
+
         every { communication.askInput<List<Host>>("Insert hosts")
         } answers { defaultHostList() }
         every { communication.askInput<String>("Insert name")
@@ -23,10 +20,9 @@ class ApplicationTest: StringSpec() {
         every { communication.askInput<LocalDateTime>("Insert time")
         } answers { LocalDateTime.of(2019, Month.MAY, 5, 19, 5, 0) }
 
+        val sut = Application(communication, storage)
 
         "Application creates a meeting with input from the communication port" {
-            val sut = Application(communication)
-
             val result = sut.createMeeting()
 
             result shouldBe Meeting(
@@ -36,5 +32,28 @@ class ApplicationTest: StringSpec() {
                 LocalDateTime.of(2019, Month.MAY, 5, 19, 5, 0))
         }
 
+        "Application can create meeting using participants from the last meeting" {
+            every { storage.restoreLatest() } answers { Meeting (
+                hosts = listOf(
+                    Host("name1", "surname1", true),
+                    Host("name2", "surname2", false),
+                    Host("name3", "surname3", false),
+                    Host("name4", "surname4", true)
+                )
+            )}
+
+            val result = sut.createMeetingUsingLatestParticipants()
+
+            result shouldBe Meeting(
+                listOf(
+                    Host("name1", "surname1", true),
+                    Host("name2", "surname2", false),
+                    Host("name3", "surname3", false),
+                    Host("name4", "surname4", true)
+                    ),
+                "some name",
+                "some location",
+                LocalDateTime.of(2019, Month.MAY, 5, 19, 5, 0))
+        }
     }
 }
